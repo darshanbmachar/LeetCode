4a82de8ef41cbe3c9fa886d45ddf2ea0138482d5
darshanbmachar <165198848+darshanbmachar@users.noreply.github.com>
2026-01-30 19:43:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,13 @@
+class Solution {
+    public static int reverse(int x) {
+    int rev = 0;
+
+    while (x != 0) {
+        int digit = x % 10;
+        rev = rev * 10 + digit; // overflow allowed
+        x /= 10;
+    }
+
+    return rev;
+}
+}
